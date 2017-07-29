eeb7fab07f564df51584188e024723a4358ecf41
John Erling Blad <jeblad@gmail.com>
2017-07-29 20:27:24 +0200

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+/* jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-markdownlint' );
+	grunt.loadNpmTasks('grunt-merge-json');
+
+	grunt.initConfig( {
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		},
+		markdownlint: {
+			all: [
+				'**/*.md',
+				'!node_modules/**'
+			]
+		},
+		"merge-json": {
+			options: {
+				replacer: function( key, value ) {
+					if ( key === '@metadata' ) {
+						return undefined;
+					}
+					return value;
+				}
+			},
+			"snippets": {
+				files: {
+					"snippets/lua.json": [ "src/lua/*.json" ]
+				}
+			}
+		}
+	 } );
+
+	grunt.registerTask( 'lint',
+		[
+			'jsonlint',
+			'markdownlint'
+		] );
+	grunt.registerTask( 'test',
+		[
+			'lint'
+		] );
+	grunt.registerTask( 'merge',
+		[
+			'merge-json'
+		] );
+	grunt.registerTask( 'default',
+		[
+			'merge',
+			'test'
+		] );
+};
